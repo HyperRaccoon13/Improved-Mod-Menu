92faa3dfd4cc86cd6aa39b3e6b941d4991b3c176
HyperRaccoon13 <daraccoon133@gmail.com>
2024-08-14 04:23:41 +0100

    chaos
    theme stuff
    altho it broke some how

--- loaders/ConfigLoader.py
+++ loaders/ConfigLoader.py
@@ -4,9 +4,9 @@ import customtkinter as ctk
 class ConfigLoader:
     def __init__(self, configFile):
         self.configFile = configFile
-        self.configData = self._loadConfig()
+        self.configData = self._LoadConfig()
 
-    def _loadConfig(self):
+    def _LoadConfig(self):
         try:
             with open(self.configFile, "r") as file:
                 return json.load(file)
@@ -20,12 +20,12 @@ class ConfigLoader:
             print(f"An unexpected error occurred: {e}")
             return {}
 
-    def applyConfig(self, app, windowType="main"):
+    def ApplyConfig(self, app, windowType="main"):
         if "theme" in self.configData:
             themeName = self.configData["theme"]
             if themeName in ["green", "dark-blue", "blue"]:
                 ctk.set_default_color_theme(themeName)
-            else: ctk.set_default_color_theme("themes/"+ themeName +".json")
+            else: ctk.set_default_color_theme("Improved-Mod-Menu/themes/"+ themeName +".json")
 
         if "themeMode" in self.configData:
             themeMode = self.configData["themeMode"]
@@ -38,3 +38,16 @@ class ConfigLoader:
             y = size.get("y", 600)
             app.geometry(f"{x}x{y}")
 
+    def UpdateConfig(self, key, value):
+        self.configData[key] = value
+        self._SaveConfig()
+
+    def _SaveConfig(self):
+        try:
+            with open(self.configFile, "w") as file:
+                json.dump(self.configData, file, indent=4)
+        except Exception as e:
+            print(f"An error occuurred while saving the config file: {e}")
+
+    def GetConfigValues(self, key, default=None):
+        return self.configData.get(key, default)
--- main.py
+++ main.py
@@ -1,23 +1,50 @@
+
 import customtkinter as ctk
 from loaders import ConfigLoader
 from loaders import AssetLoader
 
-assetPath = "assets/"
+assetPath = "assets"
 configPath = "config.json"
+
 
 class SettingsWindow(ctk.CTkToplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.title("Settings")
-        configLoader.applyConfig(self, windowType="settings")
+        configLoader.ApplyConfig(self, windowType="settings")
+
+
+        def ThemeModeOptionMenuCallback(value):
+            configLoader.UpdateConfig("themeMode", value)
+
+        def ThemeOptionMenuCallback(value):
+            configLoader.UpdateConfig("theme", value)
+
+        def SettingsMenuRefreshCallback():
+            pass
+
+        settingsMenuFrame = ctk.CTkFrame(self)
+        settingsMenuFrame.pack(side="left", anchor="n", padx=20, pady=20)
+
+        self.themeModeOptionMenu = ctk.CTkOptionMenu(settingsMenuFrame, values=["dark", "light", "system"], command=ThemeModeOptionMenuCallback)
+        self.themeModeOptionMenu.pack(pady=(0, 10), padx=10)
+        self.themeModeOptionMenu.set(configLoader.GetConfigValues("themeMode"))
+
+        self.themeOptionMenu = ctk.CTkOptionMenu(settingsMenuFrame, values=["blue", "dark-blue", "green"], command=ThemeOptionMenuCallback)
+        self.themeOptionMenu.pack(pady=(5, 5), padx=10)
+        self.themeOptionMenu.set(configLoader.GetConfigValues("theme"))
+
         
+        self.settingsMenuRefreshButton = ctk.CTkButton(settingsMenuFrame, text="Refresh", command=SettingsMenuRefreshCallback)
+        self.settingsMenuRefreshButton.pack(pady=(10, 0), padx=10)
+
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("HyperModMenu")
 
-        configLoader.applyConfig(self, windowType="main")
+        configLoader.ApplyConfig(self, windowType="main")
 
         settingButtonIcon = ctk.CTkImage(light_image=assetLoader.GetAsset("gears-solid"),
                                          dark_image=assetLoader.GetAsset("gears-solid"),
@@ -32,7 +59,6 @@ class App(ctk.CTk):
     def OpenSettingPannel(self):
         settingWindow = SettingsWindow(self)
         settingWindow.grab_set()
-
 
 if __name__ == "__main__":
     configLoader = ConfigLoader(configPath)
